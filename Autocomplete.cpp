acdefe85bad88b978bff3c572ca48a5002053990
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-05-18 20:07:20 +0930

    Update Autocomplete.cpp

--- Autocomplete.cpp
+++ Autocomplete.cpp
@@ -1,32 +1,29 @@
 #include "Autocomplete.h"
 
 std::vector<std::string> Autocomplete::getSuggestions(std::string partialWord){
-    std::vector<std::string> vect;
-    
     Autocomplete* pCrawl = this;
-    for (char c : partialWord) {
+    for (char c : partialWord) 
+    {
         int ind = (int)c - (int)'a';
         pCrawl = pCrawl->children[ind];
     }
+    std::vector<std::string> vect;
     suggestionsRec(vect, pCrawl, partialWord);
-
     return vect;
 }
 
 
 // Recursive function to print auto-suggestions for given node.
-void Autocomplete::suggestionsRec(std::vector<std::string> vect, Autocomplete* pCrawl, std::string partialWord)
+void Autocomplete::suggestionsRec(std::vector<std::string> &vect, Autocomplete* pCrawl, std::string partialWord)
 {
     // found a string in Trie with the given prefix
     if (pCrawl->isWordEnd==true){
-        //std::cout <<"found word! " <<partialWord << std::endl;
         vect.push_back(partialWord);
     }
     for (int i = 0; i < 26; i++){
         if (pCrawl->children[i]) {
             // child node character value
             char child = 'a' + i;
-            //std::cout << partialWord + child << std::endl;
             suggestionsRec(vect, pCrawl->children[i], partialWord + child);
         }
     }
