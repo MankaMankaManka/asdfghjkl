65ae2c349ac75cd403c584baf166f41a54182d6b
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-10-09 23:55:04 +1030

    Update PrefixMatcher.h

--- PrefixMatcher.h
+++ PrefixMatcher.h
@@ -1,5 +1,6 @@
 #ifndef PREFIXMATCHER_H
 #define PREFIXMATCHER_H
+
 #include <iostream>
 #include <vector>
 class PrefixMatcher
@@ -7,11 +8,12 @@ class PrefixMatcher
 public:
     PrefixMatcher* children[2];
     bool isWordEnd;
-    int number = -1;
+    int number;
     
     PrefixMatcher()
     {
         this->isWordEnd = false;
+        this->number = -1;
  
         for (int i = 0; i < 2; i++) {
             this->children[i] = nullptr;
@@ -22,5 +24,4 @@ public:
     void insert(std::string address, int routerNumber);
     
 };
-
 #endif
